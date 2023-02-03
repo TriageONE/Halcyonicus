b28bbf36636798b7ac6be267426f085c5dab27ad
Aron.Mantyla <aron.mantyla@gmail.com>
2023-02-03 11:16:23 -0600

    Finished the world heightmap layer

--- lib/world/world.h
+++ lib/world/world.h
@@ -1,7 +1,7 @@
 //
 // Created by Triage on 1/31/2023.
 //
-
+#import "array"
 #ifndef HALCYONICUS_WORLD_H
 #define HALCYONICUS_WORLD_H
 
@@ -26,15 +26,30 @@ private:
      * And so a heightmap was created, consisting of an array, but how large?
      * each tile can have up to 4 states, so 2 bits. Mountain, land, water, ocean.
      * the smallest unit i can work with is 1 byte, therefore 8 bits. 4 Tiles can be stored in each byte.
-     * a chunk is derived from a 64x64 area of tiles, or 4096 unique tiles. bar compression, this mandates exactly 512 bytes of data per chunk, for the heightmap
+     * a chunk is derived from a 64x64 area of tiles, or 4096 unique tiles. bar extra compression, this mandates exactly 1024 bytes of data per chunk, for the heightmap
      */
-    HEIGHT map[512] { OCEAN };
+    char8_t map[1024] { 0 };
+
+public:
+    //God Says: "Let there be a way for one to manipulate individual places on the master array, treating the array as a large array rather than a coordinate plane
     HEIGHT pick(int place);
     void pack(int place, HEIGHT value);
-public:
+
+    //God says: "Let there be a way for those such as yourself and your consort to work upon the map, as if it were a coordinate plane read like a book. We shall find the correct placement for you"
     void set(HEIGHT h, int x, int y);
     HEIGHT get(int x, int y);
 
+    //God says: "Let there be a way for those to see, one way in memory, and another in a monofaregraph, so that we may understand the root of our product"
+    void dump_map();
+    void out();
+
+    //God says: "And so we shall let the user poke and prod, as they know what is best and why- with only their product to show, the power is theirs to have"
+    std::array<unsigned char, 1024> copy_map();
+    void set_map(std::array<unsigned char, 1024> new_map);
+
+    //God says: "Let there be a way for those who maniplate the map at a granular or programmable level to easily interface, as reinventing the wheel is pointless and wastes your time"
+    static unsigned char compress(std::array<HEIGHT, 4>);
+    static std::array<HEIGHT, 4> uncompress(unsigned char c);
 };
 
 #endif //HALCYONICUS_WORLD_H
--- main.cpp
+++ main.cpp
@@ -1,14 +1,54 @@
 #include <iostream>
 #include "lib/world/world.h"
+#include "lib/noise/perlin.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
     HEIGHTMAP map = *new HEIGHTMAP;
-    map.set(HEIGHT::LAND, 63, 63);
-    cout << map.get(63, 63) << endl;
-    cout << map.get(62, 63) << endl;
-    cout << map.get(0, 0) << endl;
+
+    const siv::PerlinNoise::seed_type seed = 141249u;
+    const siv::PerlinNoise perlin{ seed };
+
+    for (int y = 0; y < 64; ++y)
+    {
+        for (int x = 0; x < 64; ++x)
+        {
+            const int noise = (int) (perlin.octave2D_01((x * 0.04), (y * 0.04), 2) * 4);
+            map.set((HEIGHT) noise, x, y);
+        }
+
+    }
+    map.out();
+    cout << endl;
+
+    std::array<unsigned char, 1024> pMap = map.copy_map();
+
+    std::array<HEIGHT, 4> h = HEIGHTMAP::uncompress(pMap[0]);
+    cout << "INITIAL: ";
+    for (int i = 0; i <4; i++){
+        cout << h[i];
+    }
+    cout << endl;
+
+    h[0] = HEIGHT::OCEAN;
+    h[1] = HEIGHT::WATER;
+    h[2] = HEIGHT::LAND;
+    h[3] = HEIGHT::MOUNTAIN;
+    cout << "AFTER: ";
+    for (int i = 0; i <4; i++){
+        cout << h[i];
+    }
+    cout << endl;
+    pMap[0] = HEIGHTMAP::compress(h);
+
+    cout << "SANITY: ";
+    array<HEIGHT, 4> arr = HEIGHTMAP::uncompress(pMap[0]);
+    for (int i = 0; i <4; i++){
+        cout << arr[i];
+    }
+    map.set_map(pMap);
+
+    map.dump_map();
     return 0;
 }
