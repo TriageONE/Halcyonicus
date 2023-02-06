fe38425715f6377ba578e9e90c0ba665f306e28d
aronm <aron.mantyla@gmail.com>
2023-02-05 22:00:27 -0600

    Removed the redundant inner classes of the maps present within the world and opted to instead use the base map class

--- lib/world/world.h
+++ lib/world/world.h
@@ -13,45 +13,48 @@ protected:
     std::string seed;
 
 public:
+
+    /**
+     * 1:1, Thus a new area of the world internal was to be made, a space dedicated to telling climate data about how warm or cold an area was to be.
+     * This was to be represented on a scale the same as height, with little variation in the data alone. 4 states require 2 bits, for a total of 4096 areas, requiring 1024 bytes.
+     */
+    /**
+     * 1:0:1, and with that, god created a way to call each tone of temperature, with no distinct middle ground.
+     * There shall be no true center, as perfection in zonal temperate attribution is impossible, even in the world beyond.
+     */
+     /**
+     * 1:2, Finally, a way for many maps of all nomenclature to reside under one object, so that when we are handling the art, all things reside in a single place,
+     * free from confusion.
+     */
+    MAP climatemap;
+
+    /**
+     * 0:1, And god said, let there be a way to discern between heights, as the world must know verticality in 4's
+     */
     /**
      * 0:2, And so a map of veticality was created, consisting of an array, but how large?
      * each tile can have up to 4 states, so 2 bits. Mountain, land, water, ocean.
      * the smallest unit i can work with is 1 byte, therefore 8 bits. 4 Tiles can be stored in each byte.
      * a chunk is derived from a 64x64 area of tiles, or 4096 unique tiles. bar extra compression, this mandates exactly 1024 bytes of data per chunk, for the heightmap
      */
-    class HEIGHTMAP : public MAP {
-    public:
-        /**
-         * 0:1, And god said, let there be a way to discern between heights, as the world must know verticality in 4's
-         */
-         using MAP::MAP;
-    };
+    MAP heightmap;
 
     /**
-     * 1:1, Thus a new area of the world internal was to be made, a space dedicated to telling climate data about how warm or cold an area was to be.
-     * This was to be represented on a scale the same as height, with little variation in the data alone. 4 states require 2 bits, for a total of 4096 areas, requiring 1024 bytes.
+     * 3:0, And god said there shall be a way for water and its general measurement to be decided, as the world was full of essence and vaired between place to place
      */
-    class CLIMATEMAP : public MAP {
-    public:
-        /**
-         * 1:0:1, and with that, god created a way to call each tone of temperature, with no distinct middle ground.
-         * There shall be no true center, as perfection in zonal temperate attribution is impossible, even in the world beyond.
-         */
-        using MAP::MAP;
-    };
-
     /**
-     * 1:2, Finally, a way for many maps of all nomenclature to reside under one object, so that when we are handling the art, all things reside in a single place,
-     * free from confusion.
+     * 3:1 As there was no reason to change the amount of resolution required, they kept the data the same, requiring only 4 states of being and not one more
      */
-    CLIMATEMAP climatemap;
-    HEIGHTMAP heightmap;
+    MAP saturationmap;
 
     explicit WORLD(std::string seed){
         MD5 md5;
         this->seed = seed;
         heightmap.set_seed(seed);
-        climatemap.set_seed(md5(&seed, 8));
+        std::string s1 = md5(&seed, 8);
+        climatemap.set_seed(s1);
+        std::string s2 = md5(&s1, 8);
+        saturationmap.set_seed(s2);
     }
 
     /**
@@ -72,6 +75,8 @@ public:
      * discriminating on basis of what map rather than *the* map
      */
     void generate();
+
+    void constrain() const;
 };
 
 #endif //HALCYONICUS_WORLD_H
