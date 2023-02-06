0230c5fe39f5d0835b4920a96f1e9ed4c11b1369
aronm <aron.mantyla@gmail.com>
2023-02-05 22:02:19 -0600

    improved the implementation of the options now available for vBias, weighting, roughness, and scaling

--- lib/world/map.cpp
+++ lib/world/map.cpp
@@ -8,6 +8,7 @@
 #include <bitset>
 #include <array>
 #include <utility>
+#include "../hash/md5.h"
 
 //////////////////////////
 //CLASSIFICATION BASIC RW
@@ -329,15 +330,62 @@ float MAP::get_w3() const {
 }
 
 void MAP::generate() {
-    const siv::PerlinNoise::seed_type lseed = *(int*) (seed.c_str());
-    const siv::PerlinNoise perlin{ lseed };
+    //Reinterpretation cast to integer from character array
+    int s1 = *(int*) (seed.c_str());
 
+    MD5 md5;
+    int s2 = *(int*) (md5(&seed, 8).c_str()) << 1;
+
+    const siv::PerlinNoise::seed_type pseed = s1;
+    const siv::PerlinNoise perlin{ pseed };
+    const siv::PerlinNoise::seed_type sseed = s2;
+    const siv::PerlinNoise mperlin{ sseed };
+
+    float metascalar = scalar/4 < 0.0001f ? 0.0001f : scalar/4;
+    metascalar = metascalar > 0.04f ? 0.04f : metascalar;
+
+    //Repeat for each tile within the map
     for (int y = 0; y < 64; ++y)
     {
         for (int x = 0; x < 64; ++x)
         {
-            const double noise = (perlin.octave2D_01((x * scalar), (y * scalar), 2)*4) + vBias;
-            set((char8_t) noise, x, y);
+            //The metascalar is a method to bias the entire noise pattern to create large metapatterns.
+            //This prevents static patterns that reoccur over a large area, such as creating a map where oceans really don't exist, because the granularity is too low.
+            //This is the second abstraction layer to design a trend around a larger context, like biomes wrap landmasses, so that not all landmasses are the same
+            const double metamap = (mperlin.octave2D_01((x * metascalar), (y * metascalar), 2) * 2 ) -1;
+
+            //Roughness is a measure of variation, but not random. It must also be seeded perlin noise, transforming the major perlin noise product
+            double rnoise = 0.0;
+            if (roughness != 0.0f)
+                //The roughness map is an inversely correlated, static scalar, single octave perlin noise mapping that relies on the pattern of the main world.
+                //I'm not sure, but I think if we were to render a map at 0.5 scalar with no vbias and any roughness between 0.25-0.50, we may get a pretty looking 'reflection' across the world axi
+                rnoise = (float)((perlin.octave2D_01(((-1) * x * 0.5), ((-1) * y * 0.5), 1) * 2) - 1 ) * roughness;
+            //VBias is a method if adding a constant height decrement or increment so that the likelihood of generating an ocean for example would increase linearly
+            //Scalar is also important and allows for you to kinda "zoom in or out" on the projection
+            const double noise = perlin.octave2D_01((x * scalar), (y * scalar), 2) + vBias + rnoise + metamap/4;
+
+            //Weighted average:
+            // the product of the sum of weights with the noise factor, deducted through a ladder comparator.
+
+            //If we were to use w1 and w2 raw, the cutoff would be the same or possibly lower than w0
+            //We cant affect the w* class weight variables anyways, so make 2 more virtual weights
+            float vw1 = w1+w0;
+            float vw2 = w2+vw1;
+
+            // VW2 = w0+w1+w2, therefore vw2+w3 = w0+w1+w2+w3
+            double total = noise * ( vw2 + w3 );
+
+            char8_t final = 0;
+
+            //Ladder comparator to seek within ranges, could directly cast to char but that defeats the point of weighted averaging
+            if(total >= w0 && total < vw1) {
+                final = 1;
+            } else if (total >= vw1 && total < vw2){
+                final = 2;
+            } else if (total >= vw2) {
+                final = 3;
+            }
+            set((char8_t) final, x, y);
         }
     }
 }
