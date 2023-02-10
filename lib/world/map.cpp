afdb03984a0bd2b5ff370f2c6efe57a55c896ce2
Aron.Mantyla <aron.mantyla@gmail.com>
2023-02-10 17:02:16 -0600

    Added offset for map generation

--- lib/world/map.cpp
+++ lib/world/map.cpp
@@ -343,7 +343,9 @@ void MAP::generate() {
 
     float metascalar = scalar/4 < 0.0001f ? 0.0001f : scalar/4;
     metascalar = metascalar > 0.04f ? 0.04f : metascalar;
-
+    int xoff, yoff;
+    xoff = l.getX() * 64;
+    yoff = l.getY() * 64;
     //Repeat for each tile within the map
     for (int y = 0; y < 64; ++y)
     {
@@ -352,17 +354,17 @@ void MAP::generate() {
             //The metascalar is a method to bias the entire noise pattern to create large metapatterns.
             //This prevents static patterns that reoccur over a large area, such as creating a map where oceans really don't exist, because the granularity is too low.
             //This is the second abstraction layer to design a trend around a larger context, like biomes wrap landmasses, so that not all landmasses are the same
-            const double metamap = (mperlin.octave2D_01((x * metascalar), (y * metascalar), 2) * 2 ) -1;
+            const double metamap = (mperlin.octave2D_01((x * metascalar) + xoff, (y * metascalar) + yoff, 2) * 2 ) -1;
 
             //Roughness is a measure of variation, but not random. It must also be seeded perlin noise, transforming the major perlin noise product
             double rnoise = 0.0;
             if (roughness != 0.0f)
                 //The roughness map is an inversely correlated, static scalar, single octave perlin noise mapping that relies on the pattern of the main world.
                 //I'm not sure, but I think if we were to render a map at 0.5 scalar with no vbias and any roughness between 0.25-0.50, we may get a pretty looking 'reflection' across the world axi
-                rnoise = ((perlin.octave2D_01(((-1) * x * 0.5), ((-1) * y * 0.5), 1) * 2) - 1 ) * roughness;
+                rnoise = ((perlin.octave2D_01(((-1) * (x * 0.5)) + xoff, ((-1) * (y * 0.5)) + yoff, 1) * 2) - 1 ) * roughness;
             //VBias is a method if adding a constant height decrement or increment so that the likelihood of generating an ocean for example would increase linearly
             //Scalar is also important and allows for you to kinda "zoom in or out" on the projection
-            const double noise = perlin.octave2D_01((x * scalar), (y * scalar), 2) + vBias + rnoise + metamap/4;
+            const double noise = perlin.octave2D_01((x * scalar) + xoff, (y * scalar) + yoff, 2) + vBias + rnoise + metamap/4;
 
             //Weighted average:
             // the product of the sum of weights with the noise factor, deducted through a ladder comparator.
