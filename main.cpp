5615d62afe0c7a4dc64611e53923b30b804d7205
Aron.Mantyla <aron.mantyla@gmail.com>
2023-03-05 20:46:30 -0600

    New test proving that file ops are working correctly

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <fstream>
 #include "lib/world/world.h"
 #include "lib/noise/perlin.h"
+#include "lib/world/region.h"
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 #include <Windows.h>
@@ -17,21 +19,66 @@ int main() {
         setvbuf(stdout, nullptr, _IOFBF, 2000);
     #endif
 
-    WORLDCOORD wc = WORLDCOORD(0,0,0);
-    WORLD w = WORLD("lole", wc);
-    w.constrain();
-    MAP *h = w.getHeightmap();
-    //today i learned the diff between -> and .
-    //one is for pointers, one is for members
-    //pointers are fun and easy to use, and you should use them more
-    //If you dont use pointers, you will copy data rather than get the data
+    string seed = "hyperion";
+
+    if(!REGION::checkForDirectoryStructure()){
+        cout << "Creating directories..." << endl;
+        REGION::createDirectories();
+    }
+
+    vector<REGIONCOORD> existenceCache;
+
+    for (int x = 0; x < 16; x++){
+        for (int y = 0; y < 16; y++){
+            WORLDCOORD wc = WORLDCOORD(x,y,0);
+            WORLD w = WORLD(seed, wc);
+            w.constrain();
+            MAP *h = &w.heightmap;
+            //Larger values result in a more zoomed out display
+            h->setScalar(0.0075);
+            h->setRoughness(0.08);
+
+            w.generate();
+
+            REGIONCOORD rc = wc.getRegionCoordinates();
+            for (REGIONCOORD r : existenceCache){
+                int x1,y1;
+                x1=r.getX();
+                y1=r.getY();
+                if (rc.getY() == y1 && rc.getX() == x1){
+                    bool exists = REGION::regionExists(wc.getRegionCoordinates());
+
+                    if (!exists){
+                        filesystem::path path = REGION::parseFullPathFromRegionCoord(wc.getRegionCoordinates());
+                        cout << "Creating empty world with path " << path << endl;
+                        REGION::createEmptyWorld(path);
+                    }
+                    existenceCache.push_back(rc);
+                }
+            }
 
 
-    //Larger values result in a more zoomed out display
-    h->setScalar(0.01);
-    h->setRoughness(0.04);
-    w.generate();
-    cout << endl;
+            string w1h, w2h;
+
+            w1h = w.getRawHash();
+            REGION::writeChunk(&w);
+
+            WORLD w2(seed, wc);
+            REGION::readChunk(&w2);
+            w2h = w2.getRawHash();
+
+            cout << "WC_" << x << "_" << y << ": \n\t" << w1h << ", \n\t" << w2h << ";\n\t" << ((w1h == w2h) ? "MATCH" : "NO_MATCH") << endl;
+        }
+    }
+
+
+
+
+
+
+
+
+
 
     return 0;
 
