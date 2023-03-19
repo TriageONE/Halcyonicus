b65fef31a337c2a98306918c1b82c655a98ddb4a
aronm <aron.mantyla@gmail.com>
2023-03-19 13:11:08 -0500

    replaced JSON index with a collection of dynablobs instead to speed up processing of entity updates and simplify indexing of attributes

--- lib/entity/entity.cpp
+++ lib/entity/entity.cpp
@@ -9,20 +9,42 @@ LOCATION ENTITY::getLocation() {
     return this->location;
 }
 
+std::string ENTITY::getType() {
+    return this->type;
+}
+
+std::string ENTITY::getUniqueID() {
+    return this->uniqueID;
+}
+
+
 void ENTITY::setLocation(LOCATION l) {
     this->location = l;
 }
 
-//TODO: GETER DONE.. SOON
-std::string ENTITY::compileToPCAS() {
-    std::stringstream pcas;
-
-    /**
-     * Base entity format
-     */
-    return std::string();
+void ENTITY::setType(std::string t) {
+    this->type = std::move(t);
 }
 
+void ENTITY::setUniqueID(std::string uuid) {
+    this->uniqueID = std::move(uuid);
+}
+
+DYNABLOB *ENTITY::getAttribute(const std::string& attribute) {
+    std::map<std::string,DYNABLOB>::iterator it;
+    it = attributes.find(attribute);
+    if (it == attributes.end()) return nullptr;
+    return &attributes.find(attribute)->second;
+}
+
+void ENTITY::setAttribute(DYNABLOB data, std::string attribute) {
+    attributes.erase(attribute);
+    attributes.insert( std::pair<std::string, DYNABLOB> (attribute, data) );
+}
+
+std::map<std::string, DYNABLOB> ENTITY::getAllAttributes() {
+    return this->attributes;
+}
 
 
 
