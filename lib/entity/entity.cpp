8a75e6aa4b599f8b525f1f30b0056f609b43fba7
Aron.Mantyla <aron.mantyla@gmail.com>
2023-04-07 10:57:43 -0500

    new serialization schema

--- lib/entity/entity.cpp
+++ lib/entity/entity.cpp
@@ -3,9 +3,10 @@
 //
 #include "entity.h"
 #include <utility>
+#include <chrono>
 #include <sstream>
 
-LOCATION ENTITY::getLocation() {
+ENTITYLOCATION ENTITY::getLocation() {
     return this->location;
 }
 
@@ -13,12 +14,8 @@ std::string ENTITY::getType() {
     return this->type;
 }
 
-std::string ENTITY::getUniqueID() {
-    return this->uniqueID;
-}
 
-
-void ENTITY::setLocation(LOCATION l) {
+void ENTITY::setLocation(ENTITYLOCATION l) {
     this->location = l;
 }
 
@@ -26,15 +23,15 @@ void ENTITY::setType(std::string t) {
     this->type = std::move(t);
 }
 
-void ENTITY::setUniqueID(std::string uuid) {
-    this->uniqueID = std::move(uuid);
-}
-
 DYNABLOB *ENTITY::getAttribute(const std::string& attribute) {
     std::map<std::string,DYNABLOB>::iterator it;
     it = attributes.find(attribute);
     if (it == attributes.end()) return nullptr;
     return &attributes.find(attribute)->second;
+}
+
+bool ENTITY::getErrored() {
+    return errored;
 }
 
 void ENTITY::setAttribute(DYNABLOB data, std::string attribute) {
@@ -46,5 +43,62 @@ std::map<std::string, DYNABLOB> ENTITY::getAllAttributes() {
     return this->attributes;
 }
 
+string ENTITY::serialize(ENTITY entity){
+    // Ignore the type, just worry about extraneous data and location
 
+    ENTITYLOCATION el = entity.getLocation();
+    cfloat x(0), y(0), z(0);
+
+    x = el.getX();
+    y = el.getY();
+    z = el.getZ();
+
+    stringstream ss;
+
+    ss << '{' << x.serialize() << y.serialize() << z.serialize() << '{';
+
+    for (pair<string, DYNABLOB> p : entity.getAllAttributes()){
+        ss << '[' << p.first << p.second.serialize() << ']';
+    }
+
+    ss << "}}";
+    return ss.str();
+
+}
+
+ENTITY ENTITY::deserialize(string entityString){
+
+    if (entityString[0] != '{'){
+        cerr << "Attempted to deserialize a malformed entityString, stuck on first char, expected \'{\', got " << entityString[0] << endl;
+        return ENTITY();
+    }
+    if (entityString[19] != '{'){
+        cerr << "Attempted to deserialize a malformed entityString, stuck on attribute section initializer, expected \'{\', got " << entityString[19] << endl;
+        return ENTITY();
+    }
+
+    int len = entityString.length();
+    if (entityString[len-1] != '}'){
+        cerr << "Attempted to deserialize a malformed entityString, stuck on last char escape, expected \'}\', got " << entityString[len-1] << endl;
+        return ENTITY();
+    }
+    if (entityString[len-2] != '}'){
+        cerr << "Attempted to deserialize a malformed entityString, stuck on attribute section escape, expected \'}\', got " << entityString[len-2] << endl;
+        return ENTITY();
+    }
+
+
+    ENTITYLOCATION el;
+    cfloat  x = cfloat::deserializeToNewCFloat(entityString.substr(1,6)),
+            y = cfloat::deserializeToNewCFloat(entityString.substr(7,12)),
+            z = cfloat::deserializeToNewCFloat(entityString.substr(13,18));
+
+    el.setX(x);
+    el.setY(y);
+    el.setZ(z);
+
+    int place = 20; // Start on the first char of the first attribute.
+    if (place == len-2) return ENTITY(el);
+
+}
 
