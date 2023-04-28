ff44b77b5f5d07f50da93629071f4c280dbb7711
Aron.Mantyla <aron.mantyla@gmail.com>
2023-04-28 11:23:53 -0500

    Transfer to Windows PC

--- main.cpp
+++ main.cpp
@@ -1,28 +1,17 @@
-#include <string>
 #include <iostream>
+#include <thread>
 
-using namespace std;
 
-int main() {
+void listener()
+{
 
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-    //Does not work for no good reason
-    #include <windows.h>
-    //_setmode(_fileno(stdout), _O_U16TEXT);
-    // Enable buffering to prevent VS from chopping up UTF-8 byte sequences
-    setvbuf(stdout, nullptr, _IOFBF, 2000);
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    DWORD mode;
-    GetConsoleMode(hConsole, &mode);
-    mode |= 0x0004; // Enable ANSI escape codes
-    SetConsoleMode(hConsole, mode);
+}
 
-    for (int i = 0; i < 256; i++) {
-        cout << "\x1b[38;5;" << i << "m" << "Hello, world!" << endl;
-    }
-#endif
+int main()
+{
+    std::thread listener_thread(listener);
+    listener_thread.detach();
 
 
     return 0;
-
 }
