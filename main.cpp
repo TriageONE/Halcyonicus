7efdc18cc3b9c3e821d892bcf0349543c0cf799a
Aron.Mantyla <aron.mantyla@gmail.com>
2023-05-18 08:59:45 -0500

    Transfer to windows

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <iostream>
 #include <thread>
+#include <unistd.h>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -12,70 +13,57 @@ int main(int argc, char* argv[])
 {
     bool quit = false;
     // Initialize SDL2 and create a window and renderer
-    std::thread t{ [&quit] () {
-        SDL_Init(SDL_INIT_VIDEO);
-        SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
-        SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-        unsigned long long frequency = SDL_GetPerformanceFrequency();
-        unsigned long long startTime = SDL_GetPerformanceCounter();
-        unsigned long long originTime = SDL_GetPerformanceCounter();
-        // Load a texture for the box
-        SDL_Surface* surface = IMG_Load("box.png");
-        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-        SDL_FreeSurface(surface);
 
-        // Define the box's position and size
-        const int ogWidth = SCREEN_WIDTH / 2 - 50, ogHeight = SCREEN_HEIGHT / 2 - 50;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    unsigned long long frequency = SDL_GetPerformanceFrequency();
+    unsigned long long startTime = SDL_GetPerformanceCounter();
+    unsigned long long originTime = SDL_GetPerformanceCounter();
+    // Load a texture for the box
+    SDL_Surface* surface = IMG_Load("box.png");
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
 
-        SDL_Rect rect = { ogWidth, ogHeight, 100, 100 };
+    // Define the box's position and size
+    const int ogWidth = SCREEN_WIDTH / 2 - 50, ogHeight = SCREEN_HEIGHT / 2 - 50;
 
-        // Define the rotation angle and rotation center
-        float angle = 0.0f;
-        int fps = 0;
-        SDL_Point center = { 50, 50 };
+    SDL_Rect rect = { ogWidth, ogHeight, 100, 100 };
 
-        // Enter the main loop
+    // Define the rotation angle and rotation center
+    float angle = 0.0f;
+    int fps = 0;
+    SDL_Point center = { 50, 50 };
 
+    // Enter the main loop
+
+    //Create the event poller in another thread
+    std::thread t( [ &startTime, &frequency, &originTime, &angle, &rect, &renderer, &texture, &center, &quit] () {
         while (!quit)
         {
             Uint64 currentTime = SDL_GetPerformanceCounter();
             double delta = ((double)(currentTime - startTime) / frequency) * 100;
             double sinceStart = ((double)(currentTime - originTime) / frequency);
-
-            //std::cout << delta << std::endl;
-
+            startTime = currentTime;
             // Handle events
-            SDL_Event event;
-            while (SDL_PollEvent(&event))
-            {
-                switch (event.type)
-                {
-                    case SDL_QUIT:
-                        quit = true;
-                        break;
-                    case SDL_WINDOWEVENT:
-                        if (event.window.event == SDL_WINDOWEVENT_MOVED)
-                        {
-                            // The window has been moved
-                            // Ignore this event and continue rendering
-                        }
 
-                }
-            }
 
             // Update the angle and wrap it around at 360 degrees
-            /*angle += 1.0f * delta * 2;
+            angle += 1.0f * delta ;
             if (angle >= 360.0f)
             {
                 angle -= 360.0f;
-            }*/
+            }
 
             int newWidth, newHeight;
             newWidth = ogWidth + (sin(sinceStart*20)*10);
-            newHeight = ogHeight + (cos(sinceStart*20)*10);
+            newHeight = ogHeight + (sin(sinceStart*20)*-10);
 
             rect.w = newWidth;
             rect.h = newHeight;
+
+            rect.x = newHeight / 2;
+            rect.y = newWidth / 2;
 
             // Clear the renderer with black
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -86,17 +74,35 @@ int main(int argc, char* argv[])
             // Present the renderer to the window
             SDL_RenderPresent(renderer);
         }
+    });
 
-        // Clean up resources
-        SDL_DestroyTexture(texture);
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-    }};
+    while (!quit){
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                case SDL_WINDOWEVENT:
+                    if (event.window.event == SDL_WINDOWEVENT_MOVED)
+                    {
+                        // The window has been moved
+                        // Ignore this event and continue rendering
+                    }
 
-    while (!quit) {
-        std::chrono::
+            }
+        }
     }
+
     t.join();
+
+    // Clean up resources
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
