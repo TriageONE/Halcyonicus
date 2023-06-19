08e11ad1572c0b8817eae103d8a17f8db4b7fc3d
Aron.Mantyla <aron.mantyla@gmail.com>
2023-06-19 12:48:12 -0500

    Now works with the new chunk type

--- lib/world/generator.h
+++ lib/world/generator.h
@@ -9,6 +9,7 @@
 #include <iostream>
 #include "../noise/perlin.h"
 #include "chunk.h"
+#include "chunk2.h"
 
 class GENERATOR{
 
@@ -20,9 +21,10 @@ public:
         float roughness = 0;
         float amplitude = 64;
         int middleGround = 64;
+        float stonePercent = 65.0;
     };
 
-    static void applyPhase1(GEN_OPTIONS options, chunk *chunk){
+    static void applyPhase1(GEN_OPTIONS options, CHUNK2 *chunk){
         // Given a seed, we should be able to downfill with perlin noise and entire chunk's worth of stone into the chunk
         int s1 = *(int*) (options.seed.c_str());
 
@@ -47,32 +49,25 @@ public:
                 //The metascalar is a method to bias the entire noise pattern to create large metapatterns.
                 //This prevents static patterns that reoccur over a large area, such as creating a map where oceans really don't exist, because the granularity is too low.
                 //This is the second abstraction layer to design a trend around a larger context, like biomes wrap landmasses, so that not all landmasses are the same
-                double metamap = ((mperlin.octave2D_01((x + xoff) * metascalar, (z + zoff) * metascalar, 2) * 2 ) - 1) / 2.0;
+                double metamap = (mperlin.octave2D_01((x + xoff) * metascalar, (z + zoff) * metascalar, 2) * 2 );
 
                 //Roughness is a measure of variation, but not random. It must also be seeded perlin noise, transforming the major perlin noise product
                 double rnoise = ((perlin.octave2D_01((x + xoff), (z + zoff) , 2) * 2) - 1 ) * options.roughness +  0.1;
                 //VBias is a method if adding a constant height decrement or increment so that the likelihood of generating an ocean for example would increase linearly
                 //Scalar is also important and allows for you to kinda "zoom in or out" on the projection
 
-                double noise = perlin.octave2D_01((x + xoff) * options.scalar, (z + zoff) * options.scalar, 2) * (rnoise + metamap);
+                double noise = perlin.octave2D_01((x + xoff) * options.scalar, (z + zoff) * options.scalar, 2) * (rnoise + ((metamap - 1)/2));
 
                 int expandedValue = (noise * options.amplitude) + options.middleGround;
-                int dirtAmount = (rnoise * 4) + 2;
+                int stoneAmount = (metamap * (options.amplitude * options.stonePercent/100));
 
-                for (short i = expandedValue; i > 0; i--){
-                    short type;
-                    if (i == expandedValue){
-                        type = 1;
-                    } else if (i <= expandedValue - dirtAmount ){
-                        type = 3;
-                    } else
-                        type = 2;
-
-                    chunk->changeCube(x, i, z, type, false);
-                }
+                short data = 0x0100 | (unsigned char) stoneAmount;
+                unsigned char height = expandedValue;
+                unsigned char pos = CHUNK2::getRelativeArrayPosition(x, z);
+                chunk->heights[pos] = height;
+                chunk->data[pos] = data;
             }
         }
-        chunk->recompileAll();
     }
 };
 
