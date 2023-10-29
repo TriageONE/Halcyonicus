2393e53873771bce97bb2f30ae65818590c319ff
aronm <aron.mantyla@gmail.com>
2023-10-29 17:41:03 -0500

    After you enable the shader, you must set the values you wish for the shader using setMat4() or whatever else it might be to actually pass that data to the GPU. If you forget this then nothing will pass and you uniforms wont process correctly

--- graphics/entity/entity3d.h
+++ graphics/entity/entity3d.h
@@ -9,14 +9,20 @@
 
 class ENTITY3D{
 public:
-    static void draw(ENTITY* entity, Shader* shader, Model* model){
+    static void draw(ENTITY* entity, Shader* shader, Model* model, glm::mat4* view, glm::mat4* projection){
         if (!model->loaded) model->Load();
         glm::mat4 m = glm::scale(glm::vec3(1, 1, 1));	// scale it
         m = glm::toMat4(glm::quat(1.0,0.0,entity->getFacing(),0.0)) * m; // Rotate it
         auto el = entity->getLocation();
-        m = glm::translate(glm::vec3(el.x / 1000.0, el.y / 1000.0, el.z)) * m; // Move it
+        m = glm::translate(glm::vec3(el.x / 1000.0,el.z, el.y / 1000.0)) * m; // Move it
+        //Enable the shader
         shader->use();
-        model->Draw(*shader, m);
+
+        //Assign the values to the shader uniforms
+        shader->setMat4("projection", *projection);
+        shader->setMat4("view", *view);
+        shader->setMat4("model", m);
+        model->Draw(*shader);
     }
 };
 #endif //HALCYONICUS_ENTITY3D_H
